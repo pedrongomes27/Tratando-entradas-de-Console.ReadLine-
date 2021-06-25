1692525a3fac3cc3ec1c07c04578776e44bf20e2
pedrongomes27 <pedro.ngomes27@hotmail.com>
2021-06-25 10:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tratando_entradas_de_Console.ReadLine__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            try
+            {
+                num1 = Convert.ToInt32(Console.ReadLine());
+                num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 + num2);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Utilize apenas números");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
